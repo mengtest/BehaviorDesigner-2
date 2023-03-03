8aadf3ea6d2cf7e075a58586e7fe4c991bad3f8f
Xianjun Yin <yinxianjun@xmfunny.com>
2023-03-03 15:13:17 +0800

    update git commands

--- Assets/Editor/GitPush.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-dir_path="#dir_path#"
-push_branch="#push_branch#"
-
-git add $dir_path
-git commit -m "#cmt_message#"
-git push origin $push_branch
--- Assets/Editor/GitSubtree.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-split_path="#split_path#"
-split_branch="#split_branch#"
-
-git subtree split --prefix=$split_path --branch $split_branch
-git push origin $split_branch
